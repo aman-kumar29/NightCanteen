4233185e38b9defc3ca43b753495d9a96987d1e4
Aman <amankumar76814@gmail.com>
2024-09-22 01:30:16 +0530

    added cron job cold start

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import { dbconnect } from './config/database.config.js';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import cron from 'node-cron'; // Add cron job for periodic health checks
+import axios from 'axios'; // For making HTTP requests
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,32 +18,46 @@ dotenv.config();
 
 dbconnect();
 
-
 const app = express();
 app.use(express.json());
 app.use(
     cors(
         {
             credentials : true,
-            origin:['http://127.0.0.1:3000'],
+            origin:['http://127.0.0.1:3000','http://localhost:3000'],
         }
     )
 );
-
 
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 const PORT = process.env.PORT || 5000;
+
 // Serve static assets in production
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+// Cron job to ping the server every 10 minutes
+cron.schedule('*/10 * * * *', async () => {
+  try {
+    const response = await axios.get(`https://canteen-web-deployed.onrender.com/api/health`);
+    console.log('Health check successful:', response.data);
+  } catch (error) {
+    console.error('Health check failed:', error.message);
+  }
+});
+
+// Create a health check endpoint
+app.get('/api/health', (req, res) => {
+  res.status(200).send('Server is healthy');
+});
+
 app.listen(
-    PORT, ()=>{
+    PORT, ()=> {
         console.log("Listening on Port ", PORT);
     }
-)
+);
