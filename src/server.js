a40a4b3551cc5feed29aa4210715d31ec68a055a
Aman <amankumar76814@gmail.com>
2024-09-22 02:12:20 +0530

    origin master

--- src/server.js
+++ src/server.js
@@ -5,14 +5,14 @@ import foodRouter from './routers/food.router.js';
 import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js'
 import { dbconnect } from './config/database.config.js';
-import path from 'path';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import cron from 'node-cron'; // Add cron job for periodic health checks
-import axios from 'axios'; // For making HTTP requests
+// import path from 'path';
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import cron from 'node-cron'; // Add cron job for periodic health checks
+// import axios from 'axios'; // For making HTTP requests
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 dotenv.config();
 
@@ -35,26 +35,26 @@ app.use('/api/orders', orderRouter);
 const PORT = process.env.PORT || 5000;
 
 // Serve static assets in production
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 // Cron job to ping the server every 10 minutes
-cron.schedule('*/10 * * * *', async () => {
-  try {
-    const response = await axios.get(`https://canteen-web-deployed.onrender.com/api/health`);
-    console.log('Health check successful:', response.data);
-  } catch (error) {
-    console.error('Health check failed:', error.message);
-  }
-});
+// cron.schedule('*/10 * * * *', async () => {
+//   try {
+//     const response = await axios.get(`http://localhost:${PORT}/api/health`);
+//     console.log('Health check successful:', response.data);
+//   } catch (error) {
+//     console.error('Health check failed:', error.message);
+//   }
+// });
 
-// Create a health check endpoint
-app.get('/api/health', (req, res) => {
-  res.status(200).send('Server is healthy');
-});
+// // Create a health check endpoint
+// app.get('/api/health', (req, res) => {
+//   res.status(200).send('Server is healthy');
+// });
 
 app.listen(
     PORT, ()=> {
